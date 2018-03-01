e3bb3c6296a36f43f84e471cc182561cfceb3bb1
caesarii <iwangqinghe@gmail.com>
2018-03-01 18:27:09 +0800

    parsedArray 嵌套 数组

--- parsedArray.js
+++ parsedArray.js
@@ -1,6 +1,7 @@
 const {log, ensure} = require('./utils')
 const jsonTokens = require('./jsonTokens')
 const Type = require('./Type')
+const parsedObject = require('./parsedObject')
 
 
 const parsedArray = (tokens, i) => {
@@ -23,7 +24,7 @@ const parsedArray = (tokens, i) => {
         } else if(type === Type.braceLeft) {
     
             // 解析对象
-            const [obj, off] = objEnd(tokens, offset)
+            const [obj, off] = parsedObject(tokens, offset)
             offset = off
             arr.push(obj)
         } else if(type === Type.number) {
@@ -61,6 +62,8 @@ if(require.main === module) {
     const [arr0, off0] = parsedArray(ts0, 0)
     log('ts0', ts0)
     log('arr0', arr0)
+    log('test 0', off0, ts0.length)
+    ensure(off0 === ts0.length, 'test parsed array 0')
     
     // test parsed Array 1
     const code1 = `
@@ -74,13 +77,18 @@ if(require.main === module) {
     ]
     `
     const ts1 = jsonTokens(code1)
-    const [arr1, off1] = parsedArray(tokens, 0)
+    const [arr1, off1] = parsedArray(ts1, 0)
     log('ts1', ts1)
     log('arr1', arr1)
-    ensure(offset === ts1.length, 'test arr end')
+    ensure(off1 === ts1.length - 1, 'test parsed array 1')
 
-    // const code3 = `[{
-    //     "name": "uga",
-    //     "data": [true, 1, false, null]
-    // }]`
+    const code2 = `[{
+        "name": "uga",
+        "data": 12
+     }, [true, 1, false, null]]`
+    const ts2 = jsonTokens(code2)
+    const [arr2, off2] = parsedArray(ts2, 0)
+    log('ts2', ts2)
+    log('arr2', arr2)
+    ensure(off2 === ts2.length - 1, 'test array 2')
 }
